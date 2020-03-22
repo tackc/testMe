fd59fa4ae7c12a1080afad62dc43186fbbbe61ba
Chris Tack <chris_tack@mac.com>
2020-03-22 14:47:59 -0700

    Begin writing Runner class

---
+++ runner.js
@@ -0,0 +1,12 @@
+class Runner {
+    constructor() {
+        this.files = [];
+    }
+
+    // Walk through directory structure to find test files and add them to this.files
+    collectFiles() {
+
+    }
+}
+
+module.exports = Runner;
